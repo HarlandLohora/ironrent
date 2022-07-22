f7ca9f5ad14c57dc2a2f76acce860a46ba93ef20
HarlandLohora <harland@lohora.com>
2022-07-22 14:10:16 -0300

    :tada: :sparkles: Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+//Hooks useState para acceder a la funcionalidad del estado de React
+//Queremos decirle a React que reactzione a los cambios que se realizan
+//en la app.
+
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Button, Spinner, Image, Skeleton, Input } from "@chakra-ui/react";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import ListaCasas from "./components/ListaCasas";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Detalle from "./components/Detalle";
 
 function App() {
+  // const [valor,setValor] = useState(valorInicial)
+  const [show, setShow] = useState(true);
+  const [data, setData] = useState([]);
+  const [dataCp, setDataCp] = useState([]);
+  const [search, setSearch] = useState("");
+
+  //useEffect --> useEffect(() => {})
+
+  //Mount, Update, Unmount
+  // Si queremos que se ejecute en cada re-renderizado
+  // useEffect(() => {
+  //   console.log("Soy el UseEffect");
+  // });
+
+  // Si queremos que se ejecute una sola vez al montarse en nuestra app
+  // Con este vamos a hacer nuestras peticiones GET
+  useEffect(() => {
+    console.log("GET DATOS");
+    fetch("https://ironbnb-m3.herokuapp.com/apartments")
+      .then((datos) => {
+        datos.json().then((casas) => {
+          console.log("Datos de la casas: ", casas);
+          setData(casas);
+          setDataCp(casas);
+          setShow(false);
+        });
+      })
+      .catch(console.log);
+    // axios
+    //   .get("https://ironbnb-m3.herokuapp.com/apartments")
+    //   .then((datos) => {
+    //     console.log("esto es axios", datos);
+    //     setData(datos.data);
+    //     setShow(false);
+    //   })
+    //   .catch(console.log);
+  }, []);
+
+  const toggleShow = () => {
+    setShow(!show);
+    console.log("El valor de who es: ", show);
+  };
+
+  //Funcion para controlar lo que sucede con el input
+  const actualizarInput = (e) => {
+    //
+    setSearch(e.target.value);
+  };
+
+  //useEffect este al pendiente del update del state del input
+  //.filter()
+  useEffect(() => {
+    console.log("Se esta actualizando");
+    const dataFiltrada = dataCp.filter((casa) => {
+      return casa.title.toLowerCase().includes(search.toLowerCase());
+    });
+    setData(dataFiltrada);
+  }, [search]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Input
+        placeholder="Buscar propiedad..."
+        value={search}
+        onChange={actualizarInput}
+      />
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/casas/:id" element={<Detalle />} />
+        <Route path="/" element={<ListaCasas data={data} />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/ListaCasas.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import Card from "./Card";
+import NoData from "./NoData";
+
+const ListaCasas = (props) => {
+  const { data } = props;
+  return (
+    <Flex flexWrap={"wrap"} justifyContent="center">
+      {/* <Button colorScheme="blue" onClick={toggleShow}>
+        Completado
+      </Button>
+      {/* Thruthy condicion && mostrarEsto 
+      {show === true && (
+        <Spinner color="orange.500" size="xl" speed="1s" thickness="4px" />
+      )} */}
+      {/* [].map((dato) => {
+        return <Componente props key/>
+      }) */}
+
+      {data.map((casa, iguana) => {
+        return <Card key={iguana} info={casa} />;
+      })}
+      {/* Operador ternario -- condicion ? Verdadero : Falso */}
+
+      {/* Thruthy -- condicion && mostrar*/}
+      {data.length === 0 && <NoData />}
+    </Flex>
+  );
+};
+
+export default ListaCasas;
